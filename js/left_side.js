53aeed44c0e72f0d208d089a327b16ae02656324
AulusCaelius <274583536@qq.com>
2018-06-17 21:14:04 +0800

    Initial commit
    手机尺寸导航开发完成80%

---
+++ js/left_side.js
@@ -0,0 +1,12 @@
+function nav_side(){
+	var left_side=document.getElementById('left_list');
+	var left_side_zhezhao=document.getElementById('zhezhao');
+	left_side.style.width="45%";
+	left_side_zhezhao.style.display="block";
+}
+function nav_hidden(){
+	var left_side=document.getElementById('left_list');
+	var left_side_zhezhao=document.getElementById('zhezhao');
+	left_side.style.width="0%";
+	left_side_zhezhao.style.display="none";
+}
